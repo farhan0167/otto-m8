8132e58547b5e6b19582bb7da718d91764ab6306
farhan0167 <afi225@nyu.edu>
2024-09-21 15:40:43 -0400

    add cors for run servers

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from workflow import RunWorkflow
 from blocks import WorkflowTemplate
 import json
@@ -20,8 +21,19 @@ workflow.initialize_resources()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/workflow_run")
 async def root():
     output = workflow.run_workflow()
+    output = json.dumps(output)
     return {"message": output}
