41f047ef3bc647f6cbe90a69966cb02ae227d110
farhan0167 <afi225@nyu.edu>
2024-10-11 18:51:21 -0400

    implement tool implementer

--- lib/src/utils/llm_tools/ollama_tool.py
+++ lib/src/utils/llm_tools/ollama_tool.py
@@ -1,6 +1,10 @@
 from utils.llm_tools.tool import Tool
+from integrations.implementer import IntegrationImplementer
 
 class OllamaTool(Tool):
+    def __init__(self) -> None:
+        super().__init__()
+        self.implements = None
     
     def process_tool(self, tool):
         self.tool_schema['function']['name'] = tool['name']
@@ -11,4 +15,11 @@ class OllamaTool(Tool):
                 "description": param['description'],
             }
             self.tool_schema['function']['parameters']['required'].append(param['name'])
+        
+        if tool.get('integrated_with') is not None:
+            tool_integration = tool['integrated_with']
+            self.implements = IntegrationImplementer().create_integration(
+                integration_type=tool_integration['core_block_type'],
+                run_config=tool_integration['run_config']
+            )
         return self.tool_schema
