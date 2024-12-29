8c84f2f93d944625d60dbe70673bd8094daf8fa2
farhan0167 <afi225@nyu.edu>
2024-12-29 13:26:58 -0500

    major:rename src to otto_backend
    change the name of src to otto_backend,will help for sdk dev

--- lib/otto_backend/db/models/users.py
+++ lib/otto_backend/db/models/users.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, ForeignKey, JSON
-from db.base import Base
+from ..base import Base
 
 class Users(Base):
     __tablename__ = "users"
