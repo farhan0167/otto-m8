94cd17497552ea495d8e5a78ec118d8a10c9d9f2
farhan0167 <afi225@nyu.edu>
2024-12-30 11:04:02 -0500

    v1: trace implementation frontend
    - this is pre material ui
    - bootstrap looks not up to mark anymore

---
+++ lib/otto_backend/routers/tracer_router.py
@@ -0,0 +1,37 @@
+import json
+
+
+from fastapi import APIRouter
+from fastapi import Request, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from db.db_engine import get_db
+from db.models import TracerDBModel, Users
+from routers.dependency import get_current_user
+
+router = APIRouter()
+
+@router.get("/trace/{id}", tags=["Tracer"])
+def get_trace(
+    id: int, 
+    db_session: Session = Depends(get_db),
+    current_user: Users = Depends(get_current_user)
+):
+    # filter by user id and template id
+    traces = db_session.query(TracerDBModel).filter(
+        TracerDBModel.user_id == current_user.id,
+        TracerDBModel.template_id == id
+    ).all()
+    response = []
+    if traces:
+        # TODO: Standard Server Response: Implement a standard response template
+        for trace in traces:
+            response.append({
+                "start_timestamp": trace.start_timestamp,
+                "end_timestamp": trace.end_timestamp,
+                "execution_time": round(trace.execution_time, 2),
+                "log": trace.log
+            })
+        return response
+    else:
+        raise HTTPException(status_code=404, detail="Trace not found")    
