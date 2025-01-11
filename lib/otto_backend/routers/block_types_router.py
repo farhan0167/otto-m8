5e7938168588bc7f93e3ca74f28c040e584e8963
farhan0167 <afi225@nyu.edu>
2025-01-11 17:14:54 -0500

    backend: way to see block code

--- lib/otto_backend/routers/block_types_router.py
+++ lib/otto_backend/routers/block_types_router.py
@@ -1,9 +1,14 @@
 import json
+import inspect
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from implementations.tasks.catalog import TaskRegistry
-from implementations.integrations.catalog import IntegrationRegistry
+from implementations import (
+    TaskRegistry,
+    TaskCatalog,
+    IntegrationCatalog,
+    IntegrationRegistry
+)
 
 router = APIRouter()
 
@@ -21,3 +26,21 @@ def get_integration_block_types():
     integration_block_types = IntegrationRegistry.get_integration_registry()
     # TODO: Standard Server Response: Implement a standard response template
     return integration_block_types
+
+@router.get("/get_block_codes", tags=["Blocks"])
+def get_source_code(core_block_type: str):
+    core_block_type = core_block_type.upper()
+    try:
+        cls = TaskCatalog[core_block_type]
+    except:
+        try:
+            cls = IntegrationCatalog[core_block_type]
+        except:
+            #raise ValueError(f"Core block type {core_block_type} is not supported.")
+            return HTTPException(status_code=404, detail=f"Core block type {core_block_type} is not supported.")
+    
+    cls = cls.get_class()
+    source_path = inspect.getfile(cls)
+    with open(source_path, 'r') as f:
+        source_code = f.read()
+    return source_code
