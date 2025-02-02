2efdf1034440329afed0325a84ae1a7d3186cdf2
farhan0167 <afi225@nyu.edu>
2025-02-02 12:28:14 -0500

    custom code
    - added skeleton for how custom code will be implemented. Only for OpenAI Chat atm.
    - Implemented the component that will show block's code. Right now, onSave, a new file based on the block's uuid as filename will be created, and registered.

--- lib/otto_backend/implementations/__init__.py
+++ lib/otto_backend/implementations/__init__.py
@@ -1,2 +1,3 @@
 from .tasks.catalog import TaskCatalog, TaskRegistry
 from .integrations.catalog import IntegrationCatalog, IntegrationRegistry
+from .custom.catalog import CustomCatalog, CustomRegistry
--- lib/otto_backend/routers/block_types_router.py
+++ lib/otto_backend/routers/block_types_router.py
@@ -1,13 +1,17 @@
 import json
 import inspect
+import ast
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
+from pydantic import BaseModel
 
 from implementations import (
     TaskRegistry,
     TaskCatalog,
     IntegrationCatalog,
-    IntegrationRegistry
+    IntegrationRegistry,
+    CustomCatalog,
+    CustomRegistry
 )
 
 router = APIRouter()
@@ -16,7 +20,9 @@ router = APIRouter()
 def get_block_types():
     task_block_types = TaskRegistry.get_task_registry()
     integration_block_types = IntegrationRegistry.get_integration_registry()
-    block_types = {**task_block_types, **integration_block_types}
+    custom_block_types = CustomRegistry.get_task_registry()
+    block_types = {**task_block_types, **integration_block_types, **custom_block_types}
+    print(json.dumps(block_types, indent=4))
     # TODO: Standard Server Response: Implement a standard response template
     return block_types
 
@@ -43,4 +49,56 @@ def get_source_code(core_block_type: str):
     source_path = inspect.getfile(cls)
     with open(source_path, 'r') as f:
         source_code = f.read()
-    return source_code
+    return {
+        "source_code": source_code,
+        "source_path": source_path
+    }
+    
+class SaveBlockCodeRequest(BaseModel):
+    source_code: str
+    source_path: str
+    file_name: str
+    core_block_type: str
+    
+def insert_to_custom_catalog(
+    block_file_name: str,
+    block_code_path: str,
+    block_code,
+    core_block_type: str
+):
+    with open(block_code_path, "r", encoding="utf-8") as f:
+        tree = ast.parse(f.read(), filename=block_code_path)
+    # Extract class names
+    class_name = [node.name for node in ast.walk(tree) if isinstance(node, ast.ClassDef)][-1]
+    
+    custom_catalog_path = "implementations/custom/catalog.py"
+    with open(custom_catalog_path, "r") as f:
+        custom_catalog_source = f.read()
+        
+    block_file_name_upper = block_file_name.upper().replace("-", "_")
+    
+    for line in custom_catalog_source.split("\n"):
+        if "#### Start: Catalog for Custom Blocks ####" in line:
+           # Add a new line below the line that contains "#### Start Insert Custom Tasks Here ####"
+           newline = f"\n    {core_block_type.upper()} = 'implementations.custom.blocks.{block_file_name}.{class_name}'"
+           custom_catalog_source = custom_catalog_source.replace(line, line + newline)
+        
+        if "CustomRegistry.add_vendor(vendor)" in line:
+            newline = f"\nCustomRegistry.add_task_to_registry_by_vendor(vendor, '{core_block_type}', CustomCatalog.{core_block_type.upper()})\n"
+            custom_catalog_source = custom_catalog_source.replace(line, line + newline)
+    with open(custom_catalog_path, "w") as f:
+        f.write(custom_catalog_source)
+        
+
+@router.post("/save_block_code", tags=["Blocks"])
+def save_source_code(request: SaveBlockCodeRequest):
+    with open(request.source_path, 'w') as f:
+        f.write(request.source_code)
+    
+    insert_to_custom_catalog(
+        block_file_name=request.file_name,
+        block_code_path=request.source_path,
+        block_code=request.source_code,
+        core_block_type=request.core_block_type
+    )
+    return Response(status_code=200, content=json.dumps({"message": "Source code saved successfully."}))
