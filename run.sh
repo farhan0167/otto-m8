ad4d490e12d1ad036a830c886087b14ed6dd468b
farhan0167 <afi225@nyu.edu>
2025-02-25 20:00:19 -0500

    remove sudo

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ done
 # Function to handle cleanup on keyboard interrupt
 cleanup() {
     echo "Caught interrupt signal. Stopping Docker Compose services..."
-    sudo docker compose down
+    docker compose down
     exit 0
 }
 
@@ -38,23 +38,23 @@ mkdir .cache
 
 
 echo "Building slim base image..."
-sudo docker build -f slim-base.Dockerfile -t farhan0167/otto-m8-slim-base:latest .
+docker build -f slim-base.Dockerfile -t farhan0167/otto-m8-slim-base:latest .
 
 echo "Building base image..."
-sudo docker build -f base.Dockerfile -t farhan0167/otto-m8-base:latest .
+docker build -f base.Dockerfile -t farhan0167/otto-m8-base:latest .
 
-sudo docker build -f server.Dockerfile -t farhan0167/otto-m8-server:latest .
+docker build -f server.Dockerfile -t farhan0167/otto-m8-server:latest .
 
 cd "../dashboard"
 
 echo "Building dashboard image..."
-sudo docker build -f Dockerfile -t farhan0167/otto-m8-dashboard:latest --build-arg CAN_DEPLOY_WORKFLOW=$CAN_DEPLOY_WORKFLOW .
+docker build -f Dockerfile -t farhan0167/otto-m8-dashboard:latest --build-arg CAN_DEPLOY_WORKFLOW=$CAN_DEPLOY_WORKFLOW .
 
 cd ".."
 
 # Use the selected Compose file to build and start the services
 echo "Building and running Docker Compose with $COMPOSE_FILE and CAN_DEPLOY_WORKFLOW=$CAN_DEPLOY_WORKFLOW..."
-sudo docker compose -f "$COMPOSE_FILE" up -d
+docker compose -f "$COMPOSE_FILE" up -d
 
 
 # Echo the react app URL
